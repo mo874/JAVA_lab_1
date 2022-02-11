6108b5624dba7d13126887071e34dc8807b6baf9
mo874 <agnieszka.block@onet.eu>
2022-02-11 22:40:45 +0100

    ćwiczenie nr 2 w lab1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Witaj świecie!");
+        int zmiennaInt = 1;
+        System.out.println("zmienna int = " + zmiennaInt);
+        /* ćw.2 */
+
+        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+
+        byte zmiennaByte = 22;
+        System.out.println("zmienna byte = " + zmiennaByte);
+
+        short zmiennaShort = 3;
+        System.out.println("zmienna short = " + zmiennaShort);
+
+        long zmiennaLong = 4;
+        System.out.println("zmienna long = " + zmiennaLong);
+
+        float zmiennaFloat = 5.55f;
+        System.out.println("zmienna float = " + zmiennaFloat);
+
+        boolean zmiennaBoolean = true;
+        System.out.println("zmienna boolean = " + zmiennaBoolean);
+
+
+        /*
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
+
 
     }
 }
-
