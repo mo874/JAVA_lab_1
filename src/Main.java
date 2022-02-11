07b3f868ff41b0e78b94d57d4baf8f2765fce665
mo874 <agnieszka.block@onet.eu>
2022-02-11 23:13:45 +0100

    ćwiczenie nr 2 w lab1

--- src/Main.java
+++ src/Main.java
@@ -29,10 +29,54 @@ public class Main {
         /*
          *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
          *    mnożenia, dziealenia i modulo na zmiennych:
-         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
          *  */
 
+        int liczbaA = 22;
+        System.out.println("liczba A = " + liczbaA);
+
+        int liczbaB = 4;
+        System.out.println("liczba B = " + liczbaB);
+        
+        int wynik1 = liczbaA + liczbaB;
+        System.out.println("A+B = " + wynik1);
+
+        int wynik2 = liczbaB - liczbaA;
+        System.out.println("B-A = " + wynik2);
+
+        int wynik3 = liczbaA - liczbaB;
+        System.out.println("A-B = " + wynik3);
+
+        int wynik4 = liczbaA * liczbaB;
+        System.out.println("A*B = " + wynik4);
+
+        int wynik5 = liczbaA / liczbaB;
+        System.out.println("A/B = " + wynik5);
+
+        int wynik6 = liczbaB%liczbaA;
+        System.out.println("B%A = " + wynik6);
+
+        double liczbaX = 10.0000001;
+        System.out.println("liczba X = " + liczbaX);
+
+        double liczbaY = 8.09;
+        System.out.println("liczba Y = " + liczbaY);
+
+        double wynik7 = liczbaX + liczbaY;
+        System.out.println("X+Y = " + wynik7);
+
+        double wynik8 = liczbaX - liczbaY;
+        System.out.println("X-Y = " + wynik8);
+
+        double wynik9 = liczbaX * liczbaY;
+        System.out.println("X*Y = " + wynik9);
+
+        double wynik10 = liczbaX / liczbaY;
+        System.out.println("X/Y = " + wynik10);
+
+        double wynik11 = liczbaX % liczbaY;
+        System.out.println("X%Y = " + wynik11);
 
     }
 }
